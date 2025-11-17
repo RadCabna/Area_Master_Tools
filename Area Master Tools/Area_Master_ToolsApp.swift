c70e0d9b210f957d9809c2096f242ee9e7f8b152
Radcabna <radcabna@gmail.com>
2025-11-17 19:09:42 +0300

    Initial Commit

---
+++ Area Master Tools/Area_Master_ToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Area_Master_ToolsApp.swift
+//  Area Master Tools
+//
+//  Created by Алкександр Степанов on 17.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct Area_Master_ToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
